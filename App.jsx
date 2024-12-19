1a7f92ede9fda8cf769d227a2ed0b14b7e33f918
Yuvraj Patil <yuvrajpatil12092004@gmail.com>
2024-12-19 23:31:42 +0530

    changed app.jsx Components to components

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from './Components/Header'
+import Header from './components/Header'
 import './App.css'
 import { Outlet } from "react-router";
 import { ThemeProvider } from "./contexts/ThemeContext";
